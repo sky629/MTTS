05aa35291d991004c538cb54186f275be35b4d74
rlawlstjd <nadas9029@gmail.com>
2014-12-04 19:49:33 +0900

    cc

--- MTTS/src/main/java/com/mju/mtts/movie/service/MovieService.java
+++ MTTS/src/main/java/com/mju/mtts/movie/service/MovieService.java
@@ -6,4 +6,9 @@ import com.mju.mtts.vo.movie.Movie;
 
 public interface MovieService {
 	List<Movie> getMovieAll(String movieSeq);
+	Movie getMovie(String movieSeq);
+	Double getGrade(String movieSeq);
+	List<String> getGenre(String movieSeq);
+	List<String> getCountry(String movieSeq);
+	String getRate(String movieSeq);
 }
