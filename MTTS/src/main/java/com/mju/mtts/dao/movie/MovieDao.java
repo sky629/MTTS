039f497de1fbb59daca82513c7a8a719bb4b81ee
rlawlstjd <nadas9029@gmail.com>
2014-12-04 19:48:00 +0900

    aa

--- MTTS/src/main/java/com/mju/mtts/dao/movie/MovieDao.java
+++ MTTS/src/main/java/com/mju/mtts/dao/movie/MovieDao.java
@@ -7,4 +7,9 @@ import com.mju.mtts.vo.movie.Movie;
 public interface MovieDao {
 	
 	List<Movie> selectAll(Movie param);
+	Movie selectDetail(Movie param);
+	Double getGrade(String param);
+	List<String> getGenre(String param);
+	List<String> getCountry(String param);
+	String getRate(String param);
 }
