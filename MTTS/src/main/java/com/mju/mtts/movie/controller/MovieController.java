cbf930c2e409d7c6b6f1d2ac5e65f652cbb88c12
Administrator <Administrator@192.168.10.110>
2014-12-05 17:23:39 +0900

    뀨~

--- MTTS/src/main/java/com/mju/mtts/movie/controller/MovieController.java
+++ MTTS/src/main/java/com/mju/mtts/movie/controller/MovieController.java
@@ -1,12 +1,5 @@
 package com.mju.mtts.movie.controller;
 
-import java.io.FileNotFoundException;
-import java.text.DateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -14,7 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
