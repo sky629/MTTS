201086f01ea8604792d19be2cdf7fd6b7f9c6916
sky629 <kkt629@gmail.com>
2014-12-06 23:52:48 +0900

    헤헿

---
+++ MTTS/src/main/java/com/mju/mtts/vo/movie/Genre.java
@@ -0,0 +1,19 @@
+package com.mju.mtts.vo.movie;
+
+public class Genre {
+	private String genreSeq;
+	private String genre;
+	
+	public String getGenreSeq() {
+		return genreSeq;
+	}
+	public void setGenreSeq(String genreSeq) {
+		this.genreSeq = genreSeq;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+}
